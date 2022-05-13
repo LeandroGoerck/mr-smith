2bc3326a540cf4856a55db2228fecac4681a6151
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-05-13 17:20:43 -0300

    first server response OK

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 // project here
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send({ status: 'OK' });
+});
 
 export default app;
